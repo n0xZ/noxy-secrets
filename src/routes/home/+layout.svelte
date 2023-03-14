5a04b5ff2cee39fa4d2fd5fc27251cbd36e21515
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-14 18:44:39 -0300

    chore: add nested layout on home

---
+++ src/routes/home/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { enhance } from '$app/forms';
+</script>
+
+<header class="p-5 border-b-2 border-gray-50 ">
+	<nav
+		class="flex flex-row items-center justify-between container mx-auto max-w-3xl  font-jost xl:text-xl text-lg"
+	>
+		<a href="/home">Home</a>
+		<ul class="flex flex-row items-center space-x-5">
+			<li>
+				<form method="post" action="/home/?logout" use:enhance>
+					<button class="p-3 rounded-lg bg-violet-400 c-white font-bold" type="submit">
+						Cerrar sesión
+					</button>
+				</form>
+			</li>
+		</ul>
+	</nav>
+</header>
+<slot />
