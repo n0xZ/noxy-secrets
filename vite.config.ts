9b820d99d4f11f6f7f0a289ebcce6a4a3b66afa9
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-14 18:28:25 -0300

    chore: add google custom fonts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import { defineConfig } from 'vite';
 import Unocss from 'unocss/vite';
 import { presetUno, presetWebFonts } from 'unocss';
 export default defineConfig({
-	plugins: [sveltekit(), Unocss({ presets: [presetUno(), presetWebFonts()] })]
+	plugins: [
+		sveltekit(),
+		Unocss({
+			presets: [
+				presetUno(),
+				presetWebFonts({ provider: 'google', fonts: { jost: 'Jost', murecho: 'Murecho' } })
+			]
+		})
+	]
 });
