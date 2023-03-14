089ef33657e5da8e1ec56b29507f825d5e5fbe19
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-14 14:55:13 -0300

    add homepage

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<header class="p-5 ">
+	<nav class="flex flex-row items-center justify-between container mx-auto max-w-3xl">
+		<a href="/">Preactyx</a>
+		<ul class="flex flex-row items-center space-x-5">
+			<li>
+				<a href="/login">Iniciar sesión</a>
+			</li>
+			<li />
+		</ul>
+	</nav>
+</header>
+<main class="h-full min-h-screen flex flex-col justify-center items-center space-y-3">
+	<h2 class="text-3xl font-bold">
+		Llegó la hora de guardar los secretos de tu proyecto, de manera efectiva y segura.
+	</h2>
+	<p>
+		Con preactyx, guardarás todas tus variables de entorno relacionadas a tu proyecto, de manera
+		eficaz, rápida y sencilla.
+	</p>
+	<a href="/login" class="underline">Comenzar ya!</a>
+</main>
