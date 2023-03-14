537157e0e94fb8b558106f7401b34920b0208ff2
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-14 18:43:51 -0300

    add project-list component that will be rendered on homepage

---
+++ src/lib/components/project/project-list.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Project } from '@prisma/client';
+	import ProjectItem from './project-item.svelte';
+
+	export let projects: Project[];
+</script>
+
+<article class="grid grid-rows-3 h-full container mx-auto gap-5 xl:p-0 p-2">
+	{#each projects as project}
+		<ProjectItem {project} />
+	{/each}
+</article>
