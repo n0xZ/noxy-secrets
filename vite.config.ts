ae5348134839507ad4cf9c7d06e2c1add5f707dd
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-14 14:55:06 -0300

    chore: add unocss preset

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-
+import Unocss from 'unocss/vite';
+import { presetUno, presetWebFonts } from 'unocss';
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), Unocss({ presets: [presetUno(), presetWebFonts()] })]
 });
