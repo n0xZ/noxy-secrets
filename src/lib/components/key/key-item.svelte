4bd269ed949f244ddecb51a5b331b7af68e0bb82
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-14 18:43:26 -0300

    chore: setup key-item component

---
+++ src/lib/components/key/key-item.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { ProjectKey } from '@prisma/client';
+	import { Disclosure, DisclosureButton, DisclosurePanel } from '@rgossiaux/svelte-headlessui';
+	import DownArrowIcon from '../icon/down-arrow.svelte';
+	import ClipboardIcon from '../icon/clipboard.svelte';
+	export let projectKey: ProjectKey;
+</script>
+
+<Disclosure let:open>
+	<DisclosureButton
+		class="flex w-full justify-between rounded-lg px-4 py-2 text-left text-sm font-medium  focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+	>
+		<span class="font-bold"> {projectKey.name}</span>
+		<DownArrowIcon classStyles={`${open ? 'rotate-180 transform' : ''}`} />
+	</DisclosureButton>
+
+	<DisclosurePanel class="p-5 flex flex-row items-center  space-x-5 	 min-h-32 h-full w-full">
+		<div class="flex flex-col   w-full space-y-2 ">
+			<label for="value">Valor actual</label>
+			•••••••••••••••
+		</div>
+
+		<ClipboardIcon text={projectKey.value} />
+	</DisclosurePanel>
+</Disclosure>
