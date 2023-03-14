63e729faa29d5ca5ddbbdf362b4a73d8d5f4d69a
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-14 18:43:40 -0300

    chore: setup key-list component that will be rendered on dynamic project page

---
+++ src/lib/components/key/key-list.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { ProjectKey } from '@prisma/client';
+	import KeyItem from './key-item.svelte';
+
+	export let projectKeys: ProjectKey[];
+</script>
+
+<article class="w-full px-4 pt-16">
+	<div class="mx-auto w-full max-w-3xl rounded-2xl bg-neutral-50 p-2">
+		{#each projectKeys as projectKey}
+			<KeyItem {projectKey} />
+		{/each}
+	</div>
+</article>
