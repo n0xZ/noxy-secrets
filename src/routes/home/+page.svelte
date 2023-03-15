a30c5cd7d13b4fc957dfb98cd2e051614cb4ccda
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-14 21:50:34 -0300

    chore: add homepage

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import ProjectList from '$lib/components/project/project-list.svelte';
+	import type { PageServerData } from './$types';
+
+	export let data: PageServerData;
+</script>
+
+<main class="container mx-auto max-w-3xl  mt-10 space-y-5">
+	<h2 class="text-center text-3xl font-bold mb-10">Proyectos actuales</h2>
+
+	{#if !data.projects.length}
+		<p>No tiene proyectos creados por el momento.</p>
+	{:else}
+		<ProjectList projects={data.projects} />
+	{/if}
+</main>
