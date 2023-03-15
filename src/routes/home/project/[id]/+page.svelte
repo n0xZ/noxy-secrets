72979a303825555c07ffa4663f057124f2a836e4
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-03-14 21:55:19 -0300

    chore: add dynamic project page

---
+++ src/routes/home/project/[id]/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { PageServerData } from './$types';
+	import { page } from '$app/stores';
+	import KeyList from '$lib/components/key/key-list.svelte';
+	let id = $page.params.id;
+	export let data: PageServerData;
+</script>
+
+<svelte:head>
+	<title>Noxy - secrets | Proyecto actual</title>
+</svelte:head>
+
+<main class="h-full container mx-auto max-w-3xl">
+	{#if !data.keys.length}
+		<p>No tienes variables de entorno declaradas en este proyecot.</p>
+	{:else}
+		<KeyList projectKeys={data.keys} />
+	{/if}
+</main>
