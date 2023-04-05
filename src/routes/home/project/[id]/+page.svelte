6a7e7f24276878a08a42c974907bd13c54c257b5
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-04-04 21:37:07 -0300

    chore: add add-key functionality

--- src/routes/home/project/[id]/+page.svelte
+++ src/routes/home/project/[id]/+page.svelte
@@ -1,16 +1,42 @@
 <script lang="ts">
-	import type { PageServerData } from './$types';
-	import { page } from '$app/stores';
+	import type { ActionData, PageServerData } from './$types';
 	import KeyList from '$lib/components/key/key-list.svelte';
-	let id = $page.params.id;
+	import CreateKeyModal from '$lib/components/modal/create-key.svelte';
+	import type { SubmitFunction } from '$app/forms';
+	import AddKey from '$lib/components/icon/add-key.svelte';
+
+	let loading = false;
+	let isOpen = false;
 	export let data: PageServerData;
+	export let form: ActionData;
+
+	const addKey: SubmitFunction = () => {
+		loading = true;
+		return async ({ update }) => {
+			loading = false;
+			await update();
+		};
+	};
+
+	const closeModal = () => {
+		isOpen = false;
+	};
 </script>
 
 <svelte:head>
 	<title>Noxy - secrets | Proyecto actual</title>
 </svelte:head>
-
-<main class="h-full container mx-auto max-w-3xl">
+<aside class="p-5 flex container mx-auto max-w-3xl flex-row justify-end">
+	<button
+		class="p-3 rounded-lg flex flex-row items-center justify-center space-x-2 bg-violet-600 c-white font-bold "
+		on:click={() => (isOpen = !isOpen)}
+	>
+		<AddKey />
+		<span>Agregar nueva llave</span></button
+	>
+</aside>
+<main class="h-full container mx-auto max-w-3xl mt-10">
+	<CreateKeyModal {isOpen} {closeModal} {form} {loading} {addKey} />
 	{#if !data.keys.length}
 		<p>No tienes variables de entorno declaradas en este proyecto.</p>
 	{:else}
