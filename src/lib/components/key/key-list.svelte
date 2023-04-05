5e72fd2a444886d4ad12e979dc8d80806e977ee0
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-04-04 21:25:10 -0300

    add space utility on key-list

--- src/lib/components/key/key-list.svelte
+++ src/lib/components/key/key-list.svelte
@@ -3,10 +3,11 @@
 	import KeyItem from './key-item.svelte';
 
 	export let projectKeys: ProjectKey[];
+
 </script>
 
 <article class="w-full px-4 pt-16">
-	<div class="mx-auto w-full max-w-3xl rounded-2xl bg-neutral-50 p-2">
+	<div class="mx-auto w-full max-w-3xl rounded-2xl bg-neutral-50 p-2 space-y-4">
 		{#each projectKeys as projectKey}
 			<KeyItem {projectKey} />
 		{/each}
