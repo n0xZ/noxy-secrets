e18dc15384493996ea18acc2f35f185473ce051b
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-04-09 10:50:06 -0300

    chore: rename page file as server file and add deleteKey action

---
+++ src/routes/home/project/+page.server.ts
@@ -0,0 +1,20 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import type { Actions } from './$types';
+import { prisma } from '$lib/server/prisma';
+
+export const load: PageServerLoad = async () => {
+	throw redirect(302, '/home');
+};
+
+export const actions: Actions = {
+	deleteKey: async ({ request }) => {
+		const formData = await request.formData();
+		const id = formData.get('keyId') as string;
+		console.log(id);
+		const deletedProject = await prisma.projectKey.delete({ where: { id } });
+		return {
+			deletedProject
+		};
+	}
+};
