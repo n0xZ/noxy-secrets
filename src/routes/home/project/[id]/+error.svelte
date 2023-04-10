99e5f62ffb468b82ecc0d7b4bb4cdd11693b9760
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-04-10 10:49:33 -0300

    chore: add error page on dynamic route

---
+++ src/routes/home/project/[id]/+error.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Ocurrió un error al cargar las variables de entorno</title>
+</svelte:head>
+<section class="min-h-screen h-full flex flex-col justify-center items-center space-y-3 ">
+	<h2 class="text-4xl">Ha ocurrido un error 😢</h2>
+	<p>Por favor, inténtelo más tarde.</p>
+</section>
