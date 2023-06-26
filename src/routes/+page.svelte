43302e0ec67f0c2b6c49a40948cf2f25c169d872
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-26 15:52:16 -0300

    chore: update landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
+<script>
+
+	import Paragraph from "$components/ui/paragraph.svelte";
+
+</script>
 <svelte:head>
 	<title>Bienvenido a Noxy - secrets!</title>
 </svelte:head>
 <header class="p-5 ">
 	<nav
-		class="flex flex-row items-center justify-between container mx-auto  font-jost max-w-3xl xl:text-xl text-lg"
+		class="flex flex-row items-center justify-between container mx-auto  font-jost max-w-3xl text-foreground/70"
 	>
-		<a href="/">Noxy - secrets</a>
+		<a href="/" class="font-bold">Noxy - secrets</a>
 		<ul class="flex flex-row items-center space-x-5">
 			<li>
 				<a href="/login">Unete ya!</a>
@@ -17,13 +22,13 @@
 <main
 	class="h-full min-h-screen flex flex-col justify-center items-center space-y-3 container mx-auto"
 >
-	<h2 class="xl:text-3xl text-2xl text-center font-bold">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
 		Llegó la hora de guardar los secretos de tu proyecto, de manera efectiva y segura.
-	</h2>
-	<p class="text-center ">
+	</h1>
+	<Paragraph classStyles="text-center ">
 		Con Noxy - secrets , guardarás todas tus variables de entorno relacionadas a tu proyecto, de
 		manera eficaz, rápida y sencilla.
-	</p>
+	</Paragraph>
 	<a href="/login" class="underline">Comenzar ya!</a>
 </main>
 <footer class="p-5  text-center text-lg">
