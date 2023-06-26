8622fa63527a5fb5c82e2b6674dc827498666eeb
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-26 15:57:14 -0300

    chore: setup error page on login

---
+++ src/routes/login/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Button from '$components/ui/button.svelte';
+	import Paragraph from '$components/ui/paragraph.svelte';
+
+	const refreshPage = () => {
+		window.location.reload();
+	};
+</script>
+
+<main class="h-screen flex flex-col justify-center space-y-5 container mx-auto max-w-4xl">
+	<h2
+		class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-center"
+	>
+		Ups! Al parecer ocurrió un error 😥
+	</h2>
+	<Paragraph classStyles="text-center opacity-70">{$page.error?.message}</Paragraph>
+	<Button on:click={refreshPage}>Refrescar página</Button>
+</main>
