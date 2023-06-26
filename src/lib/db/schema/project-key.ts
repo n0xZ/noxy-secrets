dd707d398696ee63c6e82f76c0a241aeea2cf43f
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-26 18:01:38 -0300

    add value field

--- src/lib/db/schema/project-key.ts
+++ src/lib/db/schema/project-key.ts
@@ -4,7 +4,10 @@ import { projects } from './project';
 export const projectKeys = mysqlTable('ProjectKey', {
 	id: serial('id').primaryKey(),
 	name: varchar('name', { length: 191 }),
+	value: varchar('value', { length: 191 }),
 	createdAt: timestamp('createdAt')
 });
 
-export const projectKeyRelation = relations(projectKeys, ({ one }) => ({ project: one(projects) }));
+export const projectKeyRelation = relations(projects, ({ one }) => ({
+	relatedProject: one(projects, { fields: [projects.id], references: [projects.id] })
+}));
