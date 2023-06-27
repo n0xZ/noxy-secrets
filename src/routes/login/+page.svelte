3f62bd7bb6736658399cdf8561577125de896037
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-27 18:47:00 -0300

    chore: setup login page w/ github auth

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,68 +1,21 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
+	import { signIn } from '@auth/sveltekit/client';
+	import { Github } from 'lucide-svelte';
 	import Button from '$components/ui/button.svelte';
-	import type { SubmitFunction } from '@sveltejs/kit';
-	import type { ActionData } from './$types';
-	import Input from '$components/ui/input.svelte';
-	import Label from '$components/ui/label.svelte';
-
-	export let form: ActionData;
-
-	let loading = false;
-	const loginUser: SubmitFunction = () => {
-		loading = true;
-		return async ({ update }) => {
-			loading = false;
-			await update();
-		};
-	};
 </script>
 
 <svelte:head><title>Noxy - secrets | Iniciar sesión</title></svelte:head>
-<section class="h-screen grid place-items-center font-mukta">
-	<form
-		action="/login?/login"
-		use:enhance={loginUser}
-		method="post"
-		class=" flex flex-col justify-center space-y-3 max-w-2xl w-full xl:p-0 p-1"
-	>
+<main class="h-screen grid place-items-center font-mukta">
+	<section class="flex flex-col justify-center space-y-3 max-w-xl container mx-auto border-2 p-3 border-primary/10 rounded-md h-1/2">
 		<h2
 			class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-center"
 		>
 			Inicia sesión en Secrets!
 		</h2>
-		<aside class="flex flex-col justify-center space-y-3 max-w-2xl">
-			<Label for="email">Correo electrónico</Label>
-			<Input
-				type="email"
-				name="email"
-				placeholder="janedoe@email.net"
-				disabled={loading}
-				class="px-4 py-3 rounded-lg outline-none bg-light-400 c-gray-800 max-w-2xl"
-				data-test-id="emailField"
-			/>
-			<span class="h-5 text-red-500 text-sm"
-				>{#if form?.containsErrors && form?.fields?.email}
-					<p data-test-id="emailErrors">{form?.fields?.email}</p>
-				{/if}</span
-			>
-		</aside>
-		<aside class="flex flex-col justify-center space-y-3 max-w-2xl">
-			<Label for="password">Contraseña</Label>
 
-			<Input type="password" name="password" placeholder="•••••••••••••••" disabled={loading} />
-			<span class="h-5 text-red-500 text-sm"
-				>{#if form?.containsErrors && form?.fields?.password}
-					<p data-test-id="passwordErrors">{form?.fields?.password}</p>
-				{/if}
-			</span>
-		</aside>
-		<Button type="submit" disabled={loading}>{loading ? 'Cargando...' : 'Iniciar sesión'}</Button>
-		<span class="h-5 text-red-500 text-sm" data-test-id="externalErrors"
-			>{#if form?.containsErrors && form?.externalErrors}
-				<p>{form?.externalErrors}</p>
-			{/if}
-		</span>
-		<a href="/register" class="text-center underline">No tengo una cuenta</a>
-	</form>
-</section>
+		<Button class="flex flex-row items-center space-x-2" on:click={() => signIn('github', { callbackUrl: 'http://localhost:5173/home' })}>
+			<Github class="h-6 w-6" />
+			<span>Iniciar sesión desde Github</span></Button
+		>
+	</section>
+</main>
