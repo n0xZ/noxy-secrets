c48db5c72c81e49654fcc92854b949b777dca62b
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-27 18:56:38 -0300

    remove logout action from home parent route

--- src/routes/home/+page.server.ts
+++ src/routes/home/+page.server.ts
@@ -1,18 +1,12 @@
 import { redirect } from '@sveltejs/kit';
 import { prisma } from '$lib/server/prisma';
-import type { Actions, PageServerLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	const userId = locals.userId;
-	if (!userId) throw redirect(302, '/');
+	const userSess = await locals.getSession();
+	if (!userSess) throw redirect(302, '/');
 	const userProjects = await prisma.project.findMany({
-		where: { user: { id: userId } }
+		where: { user: { email: userSess.user?.email ?? '' } }
 	});
-	return { projects: userProjects };
-};
-export const actions: Actions = {
-	default: async ({ cookies }) => {
-		cookies.delete('user-session');
-		throw redirect(302, '/');
-	}
+	return { projects: userProjects, session: userSess };
 };
