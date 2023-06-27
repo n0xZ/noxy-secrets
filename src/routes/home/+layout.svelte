2c2b144db532d031189e1e8f3b341910c03d995c
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-27 18:47:15 -0300

    remove logout form and replace it with client-sign signOut feat.

--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -1,29 +1,24 @@
 <script>
-	import { enhance } from '$app/forms';
+	import { signOut } from '@auth/sveltekit/client';
+	import Button from '$components/ui/button.svelte';
 	import HomeMenu from '$lib/components/menu/home.svelte';
 </script>
 
-<header class="p-5 border-b-2 border-gray-50 ">
-	<nav
-		class="flex flex-row items-center justify-between container mx-auto max-w-3xl  font-jost xl:text-xl text-lg"
-	>
+<header class="p-5 border-b-2 border-gray-50">
+	<nav class="flex flex-row items-center justify-between container mx-auto max-w-4xl">
 		<a href="/home">Home</a>
 
-		<ul class="xl:flex lg:flex hidden flex-row items-center space-x-5 ">
+		<ul class="xl:flex lg:flex hidden flex-row items-center space-x-5">
 			<a href="/home/project/create">Crear nuevo proyecto</a>
-			<li>
-				<form method="post" action="/home/?logout" use:enhance>
-					<button
-						class="p-3 rounded-lg bg-violet-400 c-white font-bold"
-						type="submit"
-						value="LOGOUT"
-					>
-						Cerrar sesión
-					</button>
-				</form>
-			</li>
+
+			<Button
+				type="submit"
+				value="LOGOUT"
+				on:click={() => signOut({ redirect: true, callbackUrl: 'http://localhost:5173' })}
+				>Cerrar sesión</Button
+			>
 		</ul>
-				<HomeMenu/>
+		<HomeMenu />
 	</nav>
 </header>
 <slot />
