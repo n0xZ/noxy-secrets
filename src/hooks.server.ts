289369c514c7c35fbffbcdee9a0cf838b4b23c85
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-27 17:32:56 -0300

    chore: add prisma adapter

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,11 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/core/providers/github';
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
+import { prisma } from '$lib/server/prisma';
 
 export const handle = SvelteKitAuth({
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		// @ts-ignore
 		GitHub({
