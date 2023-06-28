a5d3aafae26f3aae670445ebe5f2f4d137c59e11
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-27 22:07:38 -0300

    chore: add new paragraph styles on homepage

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import ProjectList from '$lib/components/project/project-list.svelte';
+	import Paragraph from '$components/ui/paragraph.svelte';
+
 	import type { PageServerData } from './$types';
 
 	export let data: PageServerData;
@@ -9,12 +11,16 @@
 	<title>Noxy - secrets | Mis proyectos actuales</title>
 </svelte:head>
 
-<main class="container mx-auto max-w-3xl  mt-10 space-y-5">
-	<h2 class="text-center text-3xl font-bold mb-10">Proyectos actuales</h2>
+<main class="container mx-auto max-w-3xl mt-10 space-y-5">
+	<h2
+		class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-center mb-3"
+	>
+		Proyectos actuales
+	</h2>
 
 	{#if !data.projects.length}
 		<section class="h-full w-full grid place-items-center">
-			<p>No tiene proyectos creados por el momento.</p>
+			<Paragraph >No tiene proyectos creados por el momento.</Paragraph>
 		</section>
 	{:else}
 		<ProjectList projects={data.projects} />
