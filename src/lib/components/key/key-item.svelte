f15490c41224e2cd41fa8f6d8c17a32ecbd1ec7a
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-28 00:39:46 -0300

    chore: replace headlessui disclosure with shadcn ui accordion

--- src/lib/components/key/key-item.svelte
+++ src/lib/components/key/key-item.svelte
@@ -1,34 +1,30 @@
 <script lang="ts">
 	import type { ProjectKey } from '@prisma/client';
-	import { Disclosure, DisclosureButton, DisclosurePanel } from '@rgossiaux/svelte-headlessui';
-	import DownArrowIcon from '../icon/down-arrow.svelte';
+
 	import ClipboardIcon from '../icon/clipboard.svelte';
 	import DeleteIcon from '../icon/delete.svelte';
 	import { enhance } from '$app/forms';
+	import { AccordionContent, AccordionItem, AccordionTrigger } from '$components/ui/accordion';
+	import Label from '$components/ui/label.svelte';
+
 	export let projectKey: ProjectKey;
 
 	$: id = projectKey.id;
 </script>
 
-<Disclosure let:open>
-	<DisclosureButton
-		class="flex w-full justify-between rounded-lg px-4 py-2 text-left text-sm font-medium  focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
-	>
-		<span class="font-bold"> {projectKey.name}</span>
-		<DownArrowIcon classStyles={`${open ? 'rotate-180 transform' : ''}`} />
-	</DisclosureButton>
+<AccordionItem value={'item' + projectKey.name} class="w-full">
+	<AccordionTrigger class="font-bold">{projectKey.name}</AccordionTrigger>
+	<AccordionContent class="flex flex-row justify-between items-center w-full">
+		<Label for="value">Valor actual: •••••••••••••••</Label>
 
-	<DisclosurePanel class="p-5 flex flex-row items-center  space-x-5 	 min-h-32 h-full w-full">
-		<div class="flex flex-col   w-full space-y-2 ">
-			<label for="value">Valor actual</label>
-			•••••••••••••••
-		</div>
-
-		<div class="flex flex-row items-center justify-center space-x-3">
+		<form
+			method="post"
+			action={`/home/project?/deleteKey`}
+			use:enhance
+			class="flex flex-row items-center space-x-2"
+		>
 			<ClipboardIcon text={projectKey.value} />
-			<form method="post" action={`/home/project?/deleteKey`} use:enhance>
-				<button type="submit" name="keyId" value={id}><DeleteIcon /></button>
-			</form>
-		</div>
-	</DisclosurePanel>
-</Disclosure>
+			<button type="submit" name="keyId" value={id}><DeleteIcon /></button>
+		</form>
+	</AccordionContent>
+</AccordionItem>
--- src/lib/components/key/key-list.svelte
+++ src/lib/components/key/key-list.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
 	import type { ProjectKey } from '@prisma/client';
 	import KeyItem from './key-item.svelte';
+	import { Accordion } from '$components/ui/accordion/';
 
 	export let projectKeys: ProjectKey[];
-
 </script>
 
 <article class="w-full px-4 pt-16">
-	<div class="mx-auto w-full max-w-3xl rounded-2xl bg-neutral-50 p-2 space-y-4">
+	<Accordion type="single" collapsible class="w-full">
 		{#each projectKeys as projectKey}
 			<KeyItem {projectKey} />
 		{/each}
-	</div>
+	</Accordion>
 </article>
