424b11dc966b2f233cc83676bd5d4f9f6a3767b6
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-27 22:08:22 -0300

    replace callbackUrl value with env key stored

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,18 +2,26 @@
 	import { signIn } from '@auth/sveltekit/client';
 	import { Github } from 'lucide-svelte';
 	import Button from '$components/ui/button.svelte';
+	import { env } from '$env/dynamic/public';
+	
+	const callbackUrl = env.PUBLIC_AUTH_ORIGIN_URL + '/home';
 </script>
 
 <svelte:head><title>Noxy - secrets | Iniciar sesión</title></svelte:head>
 <main class="h-screen grid place-items-center font-mukta">
-	<section class="flex flex-col justify-center space-y-3 max-w-xl container mx-auto border-2 p-3 border-primary/10 rounded-md h-1/2">
+	<section
+		class="flex flex-col justify-center space-y-3 max-w-xl container mx-auto border-2 p-3 border-primary/10 rounded-md h-1/2"
+	>
 		<h2
 			class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-center"
 		>
 			Inicia sesión en Secrets!
 		</h2>
 
-		<Button class="flex flex-row items-center space-x-2" on:click={() => signIn('github', { callbackUrl: 'http://localhost:5173/home' })}>
+		<Button
+			class="flex flex-row items-center space-x-2"
+			on:click={() => signIn('github', { callbackUrl })}
+		>
 			<Github class="h-6 w-6" />
 			<span>Iniciar sesión desde Github</span></Button
 		>
