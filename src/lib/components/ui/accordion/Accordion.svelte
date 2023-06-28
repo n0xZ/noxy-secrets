e55ce7726aeb0b873cf61cc859458e08d742db1d
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-28 00:38:43 -0300

    chore: add shadcn ui accordion

---
+++ src/lib/components/ui/accordion/Accordion.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Accordion as AccordionPrimitive } from "radix-svelte";
+
+
+	export let value: any = "single";
+</script>
+
+<AccordionPrimitive.Root bind:value {...$$restProps}>
+	<slot />
+</AccordionPrimitive.Root>
