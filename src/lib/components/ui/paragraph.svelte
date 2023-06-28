e9ff54f85ce2c0649ce1de4f36d4f6df64e233d1
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-27 21:51:28 -0300

    add classStyle default value

--- src/lib/components/ui/paragraph.svelte
+++ src/lib/components/ui/paragraph.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-export let classStyles: string | null
+export let classStyles:string | null= null
 </script>
 
 <p class={`leading-7 [&:not(:first-child)]:mt-6 ${classStyles}` }>
