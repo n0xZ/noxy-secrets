2c044f52c7a79daf251ce5b8b2999d7239e4a79f
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-28 00:41:21 -0300

    chore: replace previous modal w/ shadcn one

--- src/routes/home/project/[id]/+page.svelte
+++ src/routes/home/project/[id]/+page.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
 	import type { ActionData, PageServerData } from './$types';
-	import type { SubmitFunction } from '$app/forms';
+	import type { SubmitFunction } from '@sveltejs/kit';
+	import { onMount } from 'svelte';
 	import { SvelteToast } from '@zerodevx/svelte-toast';
 	import KeyList from '$lib/components/key/key-list.svelte';
-	import CreateKeyModal from '$lib/components/modal/create-key.svelte';
+	import { page } from '$app/stores';
 	import AddKey from '$lib/components/icon/add-key.svelte';
-	import { onMount } from 'svelte';
+	import Button from '$components/ui/button.svelte';
+	import {
+		Dialog,
+		DialogContent,
+		DialogDescription,
+		DialogHeader,
+		DialogTitle,
+		DialogTrigger
+	} from '$components/ui/dialog';
+	import Input from '$components/ui/input.svelte';
+	import Label from '$components/ui/label.svelte';
+	import { enhance } from '$app/forms';
 
+	let id = $page.params.id;
 	let loading = false;
-	let isOpen = false;
 	let isMounted = false;
+	
 	export let data: PageServerData;
 	export let form: ActionData;
 
@@ -17,37 +30,81 @@
 		loading = true;
 		return async ({ update }) => {
 			loading = false;
-			isOpen = false;
 			await update();
 		};
-	};
-
-	const closeModal = () => {
-		isOpen = false;
 	};
 
 	onMount(() => {
 		isMounted = true;
 	});
-	
 </script>
 
 <svelte:head>
 	<title>Noxy - secrets | Proyecto actual: {data.projectTitle}</title>
 </svelte:head>
-
 <aside class="p-5 flex container mx-auto max-w-3xl flex-row justify-end">
-	<button
-		class="p-3 rounded-lg flex flex-row items-center justify-center space-x-2 bg-violet-600 c-white font-bold "
-		on:click={() => (isOpen = !isOpen)}
-	>
-		<AddKey />
-		<span>Agregar nueva llave</span></button
-	>
+	<Dialog modal={true} >
+		<DialogTrigger>
+			<Button class="flex flex-row items-center space-x-2">
+				<AddKey />
+				<span>Agregar nueva llave</span></Button
+			></DialogTrigger
+		>
+		<DialogContent class="max-w-xs w-full">
+			<DialogHeader>
+				<DialogTitle>Crear nueva llave</DialogTitle>
+				<DialogDescription>
+					Crea tu nueva llave para tu proyecto. Al completar el formulario, se te mostrará la llave
+					generada.
+				</DialogDescription>
+			</DialogHeader>
+			<form
+				method="post"
+				class=" flex flex-col justify-center space-y-2"
+				action="/home/project/{id}/?create-key"
+				use:enhance={addKey}
+			>
+				<aside class="flex flex-col justify-center space-y-3 max-w-2xl">
+					<Label for="email">Nombre</Label>
+					<Input
+						type="text"
+						name="name"
+						placeholder="Mi nueva key!"
+						disabled={loading}
+						class="px-4 py-3 rounded-lg outline-none bg-light-400 c-gray-800 max-w-2xl"
+						data-test-id="nameField"
+					/>
+					<span class="h-5 text-red-500 text-sm"
+						>{#if form?.containsErrors && form?.fields?.name}
+							<p data-test-id="emailErrors">{form?.fields?.name}</p>
+						{/if}</span
+					>
+				</aside>
+				<aside class="flex flex-col justify-center space-y-3 max-w-2xl">
+					<Label for="email">Valor de la llave</Label>
+					<Input
+						type="password"
+						name="value"
+						placeholder="*********"
+						disabled={loading}
+						class="px-4 py-3 rounded-lg outline-none bg-light-400 c-gray-800 max-w-2xl"
+						data-test-id="valueField"
+					/>
+					<span class="h-5 text-red-500 text-sm"
+						>{#if form?.containsErrors && form?.fields?.value}
+							<p data-test-id="emailErrors">{form?.fields?.value}</p>
+						{/if}</span
+					>
+				</aside>
+				<Button type="submit" disabled={loading}>Crear</Button>
+			
+			</form>
+		</DialogContent>
+	</Dialog>
 </aside>
 <main class="h-full container mx-auto max-w-3xl mt-10">
 	<h2 class="text-3xl text-center font-bold">{data.projectTitle}</h2>
-	<CreateKeyModal {isOpen} {closeModal} {form} {loading} {addKey} />
+
 	{#if !data.keys.length}
 		<p>No tienes variables de entorno declaradas en este proyecto.</p>
 	{:else}
